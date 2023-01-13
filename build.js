3f4cc0c7ab3357df90467a13c7600e11a1cc5425
duxianzhang <duxianzhang@meituan.com>
2023-01-13 18:04:27 +0800

    feat

--- build.js
+++ build.js
@@ -1,5 +1,7 @@
+const fs = require("fs");
 const axios = require("axios");
 
+fs.writeFileSync('./data.json', JSON.stringify({}), 'utf8')
 const url = "https://www.v2ex.com/api/nodes/list.json?fields=name,title,topics,aliases&sort_by=topics&reverse=1"
 // const url = "https://www.baidu.com";
 axios.get(url).then(console.log).catch(console.log);
