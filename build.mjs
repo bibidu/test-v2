2e0d47dbe6cac89f792d5532759c0ea0df34baea
duxianzhang <duxianzhang@meituan.com>
2023-01-13 18:42:16 +0800

    feat

--- build.mjs
+++ build.mjs
@@ -3,7 +3,9 @@ import axios from 'axios'
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
+console.time('Sleep')
 await sleep(2000)
+console.timeEnd('Sleep')
 fs.writeFileSync('./data.json', JSON.stringify({ a: 1 }), 'utf8')
 // const url = "https://www.v2ex.com/api/nodes/list.json?fields=name,title,topics,aliases&sort_by=topics&reverse=1"
 // // const url = "https://www.baidu.com";
