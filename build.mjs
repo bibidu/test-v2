f77a551abcfe6cd9fc33f0a2ded324e32432a48f
duxianzhang <duxianzhang@meituan.com>
2023-01-13 18:53:40 +0800

    feat

--- build.mjs
+++ build.mjs
@@ -2,14 +2,8 @@ import fs from 'fs'
 import axios from 'axios'
 import { execSync } from 'child_process'
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-fs.writeFileSync('./data.json', JSON.stringify({ a: 123 }), 'utf8')
-console.time('Sleep')
-sleep(1000).then(() => {
-  execSync('git add . && git commit -m "feat: auto" && git push')
-})
-console.timeEnd('Sleep')
-// const url = "https://www.v2ex.com/api/nodes/list.json?fields=name,title,topics,aliases&sort_by=topics&reverse=1"
-// // const url = "https://www.baidu.com";
-// axios.get(url).then(console.log).catch(console.log);
+const url = "https://v2ex.com/api/topics/hot.json"
+// const url = "https://www.baidu.com";
+axios.get(url).then(res => {
+  console.log(res.data)
+}).catch(console.log);
