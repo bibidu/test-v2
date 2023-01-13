e31b02bf58a054cfb5562e6d158b3d7c1b917119
duxianzhang <duxianzhang@meituan.com>
2023-01-13 18:05:57 +0800

    feat

--- build.js
+++ build.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const axios = require("axios");
+const { execSync } = require("child_process");
 
 fs.writeFileSync('./data.json', JSON.stringify({}), 'utf8')
-const url = "https://www.v2ex.com/api/nodes/list.json?fields=name,title,topics,aliases&sort_by=topics&reverse=1"
-// const url = "https://www.baidu.com";
-axios.get(url).then(console.log).catch(console.log);
+execSync('git add . && git commit -m "feat: auto" && git push')
+// const url = "https://www.v2ex.com/api/nodes/list.json?fields=name,title,topics,aliases&sort_by=topics&reverse=1"
+// // const url = "https://www.baidu.com";
+// axios.get(url).then(console.log).catch(console.log);
