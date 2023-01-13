922b91e8de004498e9369ddefa929c46893d8e1e
bibidu <787753295@qq.com>
2023-01-13 18:38:15 +0800

    Update build.js

--- build.js
+++ build.js
@@ -2,7 +2,10 @@ const fs = require("fs");
 const axios = require("axios");
 const { execSync } = require("child_process");
 
-fs.writeFileSync('./data.json', JSON.stringify({}), 'utf8')
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+await sleep(2000)
+fs.writeFileSync('./data.json', JSON.stringify({ a: 1 }), 'utf8')
 // const url = "https://www.v2ex.com/api/nodes/list.json?fields=name,title,topics,aliases&sort_by=topics&reverse=1"
 // // const url = "https://www.baidu.com";
 // axios.get(url).then(console.log).catch(console.log);
