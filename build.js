36e2e7f719a8f0ee2eab4fc764cee0f0e865b9b5
bibidu <787753295@qq.com>
2023-01-13 17:48:39 +0800

    Update build.js

--- build.js
+++ build.js
@@ -1,1 +1,7 @@
-console.log(123)
+console.log(1234)
+
+fetch('https://www.v2ex.com/api/nodes/list.json?fields=name,title,topics,aliases&sort_by=topics&reverse=1').then(res => {
+  if (res.ok) {
+    return res.json()
+  }
+}).then(console.log)
