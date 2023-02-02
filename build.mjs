efb64e9b9100d161bec2affbce6473b4ddf28ffa
bibidu <787753295@qq.com>
2023-02-02 10:58:09 +0800

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -2,13 +2,13 @@ import fs from "fs";
 import axios from "axios";
 import { execSync } from "child_process";
 
-const url = "https://v2ex.com/api/topics/hot.json";
+const url = "https://www.v2ex.com/api/topics/show.json?node_id=43";
 // const url = "https://www.baidu.com";
 axios
   .get(url)
   .then((res) => {
     console.log(res.data);
-    fs.writeFileSync("hot.json", JSON.stringify(res.data, null, 2), "utf8");
+    fs.writeFileSync("job.json", JSON.stringify(res.data, null, 2), "utf8");
     push();
   })
   .catch(console.log);
